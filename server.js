f637bec00c3348f0a16157afec790a8ee47d48ac
Manamana <allentrobson@gmail.com>
2022-12-06 16:05:49 -0700

    added helper util

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
 
+const { readAndAppend } = require('./helpers/fsUtils');
+const { writeToFile } = require('./helpers/fsUtils');
+
+const notes = require('./db/db.json');
+
 const PORT = 3001;
 
 const app = express();
@@ -12,17 +17,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
-
+app.get('/api/notes', (req, res) => res.json(notes));
 
 
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
